f05a6632e8073fb3d436e6c82400d94cf6b1fedb
AlexandrPristupa <pristupa.aleexandr@gmail.com>
2018-03-25 13:10:19 +0300

    Added codefight derectory. Added adjacentElementsProduct.js

---
+++ javascript/codefight/adjacentElementsProduct.js
@@ -0,0 +1,17 @@
+// Given an array of integers, find the pair of adjacent elements that has the largest product and return that product.
+
+function adjacentElementsProduct(inputArray) {
+    let res = 0;
+    let tmp = inputArray[0] * inputArray[1];
+    
+    for(let i = 1; i < inputArray.length; i++) {
+            res = inputArray[i] * inputArray[i + 1];
+            (res > tmp) ? tmp = res : res;
+        }
+    
+    return tmp;
+}
+
+adjacentElementsProduct([3, 6, -2, -5, 7, 3]);
+adjacentElementsProduct([-1, -2]);
+adjacentElementsProduct([5, 1, 2, 3, 1, 4]);
