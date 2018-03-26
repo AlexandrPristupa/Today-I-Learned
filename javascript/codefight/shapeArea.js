0ac79a08427fa75c6d45f5f1234b7e7bbdb49dbc
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-03-26 10:44:08 +0300

    Added shapeArea.js algoritm.

---
+++ javascript/codefight/shapeArea.js
@@ -0,0 +1,18 @@
+function shapeArea(n) {
+    let size = n * 2 - 1;
+    let res = 0;
+    let i = 1;
+
+    while (i < size) {
+        res += i;
+        i += 2;
+    }
+
+    res *= 2;
+    res +=size;
+    return res;
+}
+
+shapeArea(2);
+shapeArea(3);
+shapeArea(5);
