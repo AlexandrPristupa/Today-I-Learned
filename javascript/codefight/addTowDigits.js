c833c81f8248f7cb63eb852886e04dc08f6e38d3
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-03-28 11:06:25 +0300

    Addded two files addTowDigits.js and largestNumber.js

---
+++ javascript/codefight/addTowDigits.js
@@ -0,0 +1,7 @@
+// You are given a two-digit integer n. Return the sum of its digits.
+
+export const addTwoDigits = (n) => {
+    let str = n + '';
+    let strArr = str.split('');
+    return parseInt(strArr[0]) + parseInt(strArr[1]);
+}
---
+++ javascript/codefight/largestNumber.js
@@ -0,0 +1,11 @@
+// Given an integer n, return the largest number that contains exactly n digits.
+
+export const largestNumber = (n) => {
+    let outPut = '';
+
+    for (let i = 0; i < n; i++) {
+        outPut += 9;
+    }
+
+    return +outPut;
+}
