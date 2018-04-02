f0dd04850038a307c056bd833b881443df92c6e4
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-04-02 10:41:21 +0300

    Added centuryFormYear.js

---
+++ __tests__/codefight/centuryFromYear.spec.js
@@ -0,0 +1,11 @@
+import { centuryFromYear } from './../../javascript/codefight/centuryFromYear';
+
+describe('centuryFromYear', () => {
+  it('should be return the sum of its digits.', () => {
+    expect(centuryFromYear(1905)).toEqual(20);
+    expect(centuryFromYear(1700)).toEqual(17);
+    expect(centuryFromYear(1988)).toEqual(20);
+    expect(centuryFromYear(2000)).toEqual(20);
+    expect(centuryFromYear(2001)).toEqual(21);
+  });
+});
---
+++ javascript/codefight/centuryFromYear.js
@@ -0,0 +1,5 @@
+// Given a year, return the century it is in.
+
+export const centuryFromYear = (year) => {
+    return Math.floor((year-1)/100) + 1;
+}
