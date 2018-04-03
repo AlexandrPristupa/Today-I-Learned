7d0772d03478d0f0a2e5ca0aae5114fdf7132788
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-04-03 11:06:50 +0300

    small fix

--- __tests__/codefight/centuryFromYear.spec.js
+++ __tests__/codefight/centuryFromYear.spec.js
@@ -1,7 +1,7 @@
 import { centuryFromYear } from './../../javascript/codefight/centuryFromYear';
 
 describe('centuryFromYear', () => {
-  it('should be return the sum of its digits.', () => {
+  it('should be return the century it is in.', () => {
     expect(centuryFromYear(1905)).toEqual(20);
     expect(centuryFromYear(1700)).toEqual(17);
     expect(centuryFromYear(1988)).toEqual(20);
