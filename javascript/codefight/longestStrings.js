4dc7134dcba14e2ae9c338ae0b7806ef992444ef
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-04-06 14:31:55 +0300

    fixed codeFight longestStrings.js

--- javascript/codefight/longestStrings.js
+++ javascript/codefight/longestStrings.js
@@ -1,19 +1,26 @@
 export const longestStrings = (inputArray) => {
 
+  if(!inputArray.length || !inputArray) {
+    return [];
+  }
+
   if (inputArray.length === 1) {
     return inputArray;
   }
-
+  let stringLength = 0;
   let outPutArr = [];
 
   for (let i = 0; i < inputArray.length; i++) {
-    if (!inputArray[i + 1]) {
-      continue;
-    }
-    if (inputArray[i].length >= inputArray[i + 1].length) {
-      outPutArr.push(inputArray[i]);
+    if(inputArray[i].length > stringLength) {
+      stringLength = inputArray[i].length;
     }
   }
-  console.log(outPutArr);
+
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i].length === stringLength) {
+      outPutArr[outPutArr.length] = inputArray[i];
+    }
+  }
+
   return outPutArr;
 }
