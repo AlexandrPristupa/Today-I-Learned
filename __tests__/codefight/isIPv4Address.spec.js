aa1d59d7fc72c4d35274d4b97fd8f59e71594b8a
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-04-19 15:13:40 +0300

    Added isIPv4Address from codefights.

---
+++ __tests__/codefight/isIPv4Address.spec.js
@@ -0,0 +1,15 @@
+import { isIPv4Address } from './../../javascript/codefight/isIPv4Address';
+
+describe('find out if it satisfies the IPv4 address naming rules.', () => {
+    it('should be return true', () => {
+        expect(isIPv4Address("172.16.254.1")).toEqual(true);
+    });
+
+    it('should be return false', () => {
+        expect(isIPv4Address("172.316.254.1")).toEqual(false);
+    });
+
+    it('should be return false', () => {
+        expect(isIPv4Address(".254.255.0")).toEqual(false);
+    });
+});
