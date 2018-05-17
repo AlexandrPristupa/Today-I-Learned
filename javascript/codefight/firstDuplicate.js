71351a21198dc7235bd349c6ebf33d149acd1f4d
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-05-17 11:27:03 +0300

    Added fixed firstDuplicate from codefights

--- javascript/codefight/firstDuplicate.js
+++ javascript/codefight/firstDuplicate.js
@@ -1,25 +1,15 @@
 export const firstDuplicate = (a) => {
 
-    let num = 0;
-    let index = 0;
+    let object = {};
 
     for(let i = 0; i < a.length; i++) {
-        for(let j = i + 1; j < a.length; j++) {
-            if(a[i] === a[j]) {
-                if(num === 0) {
-                    num = a[j];
-                    index = j;
-                } else if(index > j) {
-                    num = a[j];
-                    index = j;
-                }
-            }
+        if(object.hasOwnProperty(a[i])) {
+            return a[i];
+        } else {
+            object[a[i]] = "exist";
         }
     }
 
-    if(!num && !index) {
-        return -1;
-    }
+    return -1;
+};
 
-    return num;
-}
