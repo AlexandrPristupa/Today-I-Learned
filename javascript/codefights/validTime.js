47a4de76e0f6b6da402c683277bfdb3c3f53b4de
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2018-08-31 16:07:06 +0300

    Added validTime form code fight

--- javascript/codefights/validTime.js
+++ javascript/codefights/validTime.js
@@ -1,7 +1,3 @@
-const validTime = (time) => {
-    const timeArr = time.split(':');
-
-
+export const validTime = (time) => {
+  return /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/.test(time);
 };
-
-validTime("13:58");
