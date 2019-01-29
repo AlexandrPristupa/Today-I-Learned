c6c74a7752988e3bb4b6c59295600969f595b0c4
Alexander Pristupa <pristupa.aleexandr@gmail.com>
2019-01-29 10:27:25 +0200

    Fixed es6 modules in spec.js files

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  verbose: true
+  moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+  transform: {
+    '^.+\\.(js|jsx)?$': 'babel-jest'
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  testMatch: [
+    '<rootDir>/__tests__/**/*.spec.js'
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/']
 };
