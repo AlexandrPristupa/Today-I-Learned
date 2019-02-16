f0e20e6d3ad064fe7162b4a2f20db86e5d849df0
AlexandrPristupa <pristupa.aleexandr@gmail.com>
2019-02-16 23:11:23 +0200

    added rotateArray test

--- __tests__/leetcode/top-easy/rotate.spec.js
+++ __tests__/leetcode/top-easy/rotate.spec.js
@@ -1,9 +1,14 @@
 import { rotate } from './../../../javascript/leetcode/top-easy/rotate';
+import { rotateArray } from './../../../javascript/leetcode/top-easy/rotateArray';
 
 describe('testing rotate', () => {
 
-    it('should be return true', () => {
+    it('should be return [7, 6, 5, 1, 2, 3, 4]', () => {
         expect(rotate([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([7, 6, 5, 1, 2, 3, 4]);
+    });
+
+    it('should be return [5, 6, 7, 1, 2, 3, 4]', () => {
+        expect(rotateArray([1, 2, 3, 4, 5, 6, 7], 3)).toEqual([5, 6, 7, 1, 2, 3, 4]);
     });
 
 });
--- javascript/leetcode/top-easy/rotateArray.js
+++ javascript/leetcode/top-easy/rotateArray.js
@@ -3,7 +3,7 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-export const rotate = (nums, k) => {
+export const rotateArray = (nums, k) => {
   for (let i = 0; i < k; i++) {
       let item = nums.pop();
       nums.unshift(item);
